ae31b15f2494a34b7a1b22697ad2aa0c631956b2
vivianw1 <viwu1@s.sfusd.edu>
2023-03-23 11:39:20 -0700

    challenge done

--- Assets/Scipts/SpawnManager.cs
+++ Assets/Scipts/SpawnManager.cs
@@ -14,6 +14,8 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab,GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+
     }
     
     // Update is called once per frame
